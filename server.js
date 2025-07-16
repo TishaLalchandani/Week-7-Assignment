88f94d35c51b02e1c1ae8800c169ed8e416f96ec
Tisha Lalchandani <tishalalchandani91@gmail.com>
2025-07-16 23:52:33 +0530

    Add main server setup and route mounting

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const authRoutes = require('./routes/auth');
+const protectedRoutes = require('./routes/protected');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/protected', protectedRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
